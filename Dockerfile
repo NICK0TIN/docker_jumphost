00325987e3aa8ef70372cdd674fdd9c1fe31d310
Nick Van Rompuy <nick@nick.com>
2023-12-28 09:56:52 +0100

    working

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,13 @@
 
 FROM alpine:latest
 
-
 RUN apk add --no-cache openssh-server bash openssh-client
 
-
-#ENV SSH_USER=sshuser
-#RUN adduser -D -h "/home/${SSH_USER}" "${SSH_USER}"
-
-#ARG SSH_PASSWORD
-#ARG SSH_PRIVATE_KEY
-# ENV SSH_PUBLIC_KEY test
-# ENV SSH_PRIVATE_KEY_FILE test
 ENV ONLY_ACCEPT_KEY=1
-
-
-
-# COPY conditional_config.sh /tmp/
-# RUN     chmod +x /tmp/conditional_config.sh
-# RUN     /tmp/conditional_config.sh
-# RUN     rm -f /tmp/conditional_config.sh
-
-#RUN chmod 600 /home/${SSH_USER}/.ssh/id_rsa
-
-
-# Keys to start SSHD daemon
 RUN ssh-keygen -A        
-# Keys to start SSHD daemon
-# Voeg de SSHD-configuratie toe
+
 EXPOSE 22
+
 COPY entrypoint.sh /usr/local/bin/
 ENTRYPOINT /usr/local/bin/entrypoint.sh
 
-
-
-
-#CMD ["/usr/sbin/sshd", "-D"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,22 @@
 
 
 SSH_USER=${SSH_USER:-default_user}
-
-#!/bin/bash
+MOTD_MESSSAGE=${MOTD_MESSSAGE:-"Welcome to SSHD on Alpine!"}
+echo $MOTD_MESSSAGE > /etc/motd
 SSH_USER_HOME="/home/$SSH_USER"
 SSH_USER_PUBLIC_KEY="$SSH_USER_HOME/.ssh/id_rsa.pub"
 SSH_USER_PRIVATE_KEY="$SSH_USER_HOME/.ssh/id_rsa"
-#echo $ONLY_ACCEPT_KEY
-
-
 
 if [ $ONLY_ACCEPT_KEY ]; then
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
     sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-    #sed -i 's,#HostKey /etc/ssh/ssh_host_rsa_key,#HostKey /etc/.secrets/ssh-key-jumphost ,' /etc/ssh/sshd_config && \ to add: var that chooses key file location
     sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-    #mkdir /etc/.secrets
+
 fi
 
 mkdir -p $SSH_USER_HOME/.ssh
 
-
-
  if [ ! -f $SSH_USER_PRIVATE_KEY ]; then
-    echo "it runs"
     touch $SSH_USER_PRIVATE_KEY
     ssh-keygen -q -t rsa -N '' -f $SSH_USER_PRIVATE_KEY <<<y >/dev/null 2>&1 # if not mounted via volume or secret 
   fi
@@ -33,4 +26,5 @@ mkdir -p $SSH_USER_HOME/.ssh
     chown -R $SSH_USER:$SSH_USER $SSH_USER_HOME
     chmod -R 700 $SSH_USER_HOME/.ssh/
     chmod 600 $SSH_USER_PRIVATE_KEY
+
 /usr/sbin/sshd -D -e
